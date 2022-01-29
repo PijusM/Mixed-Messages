788d62e2a8f9bf141e856b3389f873268b1b15b2
PijusM <pijus.masutko@gmail.com>
2022-01-29 20:00:13 +0200

    simple css, minor changes, added readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Simple Message Mixing script.
+
+This is a simple project, a play around with JS, HTML and CSS. 
+You are able to get a silly message, which tells you something you have to remember throughout the day.
+
+---------------------------------
+
+## Viewing the script
+
+
+*To view the result, just run index.html and it should take you in. Happy looking!*
+
+
+**Created by @PijusM**
--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const generatenumber = () =>{
     return Math.floor(Math.random() * 5);
 }
 
-const adjectives = ["boundless", "important", "poisoned", "slippery", "goofy"];
+const adjectives = ["well-made", "chubby", "irritating", "slippery", "goofy"];
 const nouns = ["governor", "glock", "pancake", "sock", "powder"];
 const verbs = ["allow", "prevent", "guide", "persuade", "convince"];
 const verbs2 = ["shut up", "attack", "belong to an anonymous person", "chat with a door", "burn your nose off"];
 
 const determinesentence = () =>{
-    document.getElementById("result").innerHTML = `You should not let a ${adjectives[generatenumber()]} ${nouns[generatenumber()]} ${verbs[generatenumber()]} you to ${verbs2[generatenumber()]}.`
+    document.getElementById("resultt").innerHTML = `You should not let a ${adjectives[generatenumber()]} ${nouns[generatenumber()]} ${verbs[generatenumber()]} you to ${verbs2[generatenumber()]}.`
 }
 
 
