119f4bfcd3f6445787028efcc64e571f046681ec
PijusM <pijus.masutko@gmail.com>
2022-01-29 19:13:08 +0200

    fixed a human error

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const generatenumber = () =>{
-    return Math.floor(Math.random() * 6);
+    return Math.floor(Math.random() * 5);
 }
 
 const adjectives = ["boundless", "important", "poisoned", "slippery", "goofy"];
 const nouns = ["governor", "glock", "pancake", "sock", "powder"];
-const verbs = ["allow", "regret", "guide", "persuade", "convince"];
+const verbs = ["allow", "prevent", "guide", "persuade", "convince"];
 const verbs2 = ["shut up", "attack", "belong to an anonymous person", "chat with a door", "burn your nose off"];
 
 const determinesentence = () =>{
     return `You should not let a ${adjectives[generatenumber()]} ${nouns[generatenumber()]} ${verbs[generatenumber()]} you to ${verbs2[generatenumber()]}.`
 }
+console.log(determinesentence())
