0f6411199e8b8b16c9908283bb78608da5d8604f
PijusM <pijus.masutko@gmail.com>
2022-02-05 11:52:11 +0200

    Fixed the ability to click a lot of times, new button shape

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ const verbs = ["allow", "prevent", "guide", "persuade", "convince"];
 const verbs2 = ["shut up", "attack", "belong to an anonymous person", "chat with a door", "burn your nose off"];
 
 const determinesentence = () =>{
-    document.getElementById("resultt").innerHTML = `You should not let a ${adjectives[generatenumber()]} ${nouns[generatenumber()]} ${verbs[generatenumber()]} you to ${verbs2[generatenumber()]}.`
+    document.getElementById("resultt").innerHTML = `You should not let a ${adjectives[generatenumber()]} ${nouns[generatenumber()]} ${verbs[generatenumber()]} you to ${verbs2[generatenumber()]}.`;
+    const button = document.querySelector('button');
+    button.disabled = true;
+}
+const disabledcolor = () =>{
+    document.getElementById("button").style.color = "#BABABA";
+    document.getElementById("button").style.opacity = 0.5;
+    document.getElementById("button").style.backgroundColor = "#7a7a7a";
+    document.getElementById("button").classList.add("disabled");
+    return false
 }
 
 
